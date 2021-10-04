abce2130f2d865de317cba2bd1fbaf46ec1a996a
eaespinoza <91565736+eaespinoza@users.noreply.github.com>
2021-10-04 15:03:00 -0500

    Add files via upload

---
+++ circledot.py
@@ -0,0 +1,14 @@
+#Intialization
+import turtle
+import random
+emily=turtle.Turtle()
+
+#Functions
+def randomDot():
+    emily.color("red")
+    emily.begin_fill()
+    emily.circle(5)
+    emily.end_fill()
+
+#Main
+randomDot()
